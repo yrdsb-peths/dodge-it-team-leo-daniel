c65ab2fadd2b239a5b0872d557061a01d704acd9
LeoXiao1 <162858575+LeoXiao1@users.noreply.github.com>
2025-04-24 08:47:04 -0400

    fix the position

--- MyWorld.java
+++ MyWorld.java
@@ -5,9 +5,9 @@ public class MyWorld extends World {
         super(600, 400, 1);
         
         Daniel D1 = new Daniel();
-        addObject(D1, 100, 250);
+        addObject(D1, 100, 300);
         
         Car C1 = new Car();
-        addObject(C1, 500, 250);
+        addObject(C1, 500, 300);
     }
 }
