180feecaa10c18e1f5494f212d47cd37d2a671b8
LeoXiao1 <162858575+LeoXiao1@users.noreply.github.com>
2025-04-25 09:07:10 -0400

    add keyboard control

--- Daniel.java
+++ Daniel.java
@@ -1,21 +1,23 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Daniel extends Actor
 {
+    
     boolean atTop = true;
     public void act()
     {
         setImage("images/pig.png");
-        if(Greenfoot.mouseClicked(null))
+        if(Greenfoot.mouseClicked(null)||Greenfoot.isKeyDown("space"))
         {
             atTop = !atTop;
-        }
-        if(atTop)
-        {
-            setLocation(100,300);
-        }
-        else
-        {
-            setLocation(100,100);
+            if(atTop)
+            {
+                setLocation(100,300);
+            }
+            else
+            {
+                setLocation(100,100);
+            }
+            Greenfoot.delay(15);
         }
     }
 }
